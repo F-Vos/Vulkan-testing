9a142dfa2181e7889ff172a4198350162c24e375
F-Vos <frankvos61@gmail.com>
2025-03-07 16:18:31 +0100

    Swap chain addition

--- first_app.hpp
+++ first_app.hpp
@@ -3,6 +3,13 @@
 #include "lve_window.hpp"
 #include "lve_Pipeline.hpp"
 #include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
+
+// std
+
+#include <memory>
+#include <vector>
+
 
 namespace lve {
 	class FirstApp {
@@ -11,10 +18,25 @@ namespace lve {
 		static constexpr int WIDTH = 800;
 		static constexpr int HEIGHT = 600;
 
+		FirstApp();
+		~FirstApp();
+
+		FirstApp(const FirstApp&) = delete;
+		FirstApp &operator=(const FirstApp&) = delete;
+
 		void run();
 	private:
+		void createPipelineLayout();
+		void createPipeline();
+		void createCommandBuffers();
+		void drawFrame();
+
 		LveWindow lveWindow{ WIDTH, HEIGHT, "Hello Vulkan" };
 		LveDevice lveDevice{ lveWindow };
-		LvePipeline LvePipeline{lveDevice, "shaders/vert.spv", "shaders/frag.spv", LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+		LveSwapChain lveSwapChain{ lveDevice, lveWindow.getExtend() };
+		std::unique_ptr<LvePipeline> lvePipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
+
 	};
 }
